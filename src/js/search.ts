536141c0761e2ad3f49b60e131539df143272892
Andrey <maoltin@gmail.com>
2022-02-23 19:26:47 +0300

    lession 1.1

--- src/js/search.ts
+++ src/js/search.ts
@@ -2,10 +2,8 @@ const search = function() {
   const input = document.querySelector('.search-block > input') as HTMLTextAreaElement;
   const searchBtn = document.querySelector('.search-block > button');
   
-  searchBtn.addEventListener('click', (event) => {
-    if (input.value) {
-      console.log(input.value);
-    }
+  searchBtn.addEventListener('click', () => {
+    console.log(input.value);
   });
 }
 search();
